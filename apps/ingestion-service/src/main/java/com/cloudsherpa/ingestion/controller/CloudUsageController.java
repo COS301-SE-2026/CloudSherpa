b68758cc62a30041fafd77e5c48bf42467c6ba0c
g3rard-j <jordaangerard10@gmail.com>
2026-04-27 08:42:16 +0200

    Producer REST controller for sending mock event

--- apps/ingestion-service/src/main/java/com/cloudsherpa/ingestion/controller/CloudUsageController.java
+++ apps/ingestion-service/src/main/java/com/cloudsherpa/ingestion/controller/CloudUsageController.java
@@ -1,5 +1,29 @@
 package com.cloudsherpa.ingestion.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cloudsherpa.ingestion.service.CloudUsageService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/events")
 public class CloudUsageController {
+    private final CloudUsageService producerService;
+
+    // CloudUsageService injected as dependency of CloudUsageController
+    public CloudUsageController(CloudUsageService producerService) {
+        this.producerService = producerService;
+    }
+
+    // POST even though no request body, produce artifact (event) => POST
+    @PostMapping("/mock")
+    public String sendMockEvent() {
+        
+        producerService.sendMockEvent();
+        return "Mock event sent";
+    }
     
 }
