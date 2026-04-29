d58c376a6d4974cc12b7dfb4b4f84cfe624fd59b
Star-shaker <norvalmegan05@gmail.com>
2026-04-29 21:09:24 +0200

    Added DTO

--- apps/analytics-engine/src/main/java/com/cloudsherpa/analytics/controller/AnalyticsController.java
+++ apps/analytics-engine/src/main/java/com/cloudsherpa/analytics/controller/AnalyticsController.java
@@ -2,6 +2,7 @@ package com.cloudsherpa.analytics.controller;
 
 import com.cloudsherpa.analytics.entity.NormalizedMetrics;
 import com.cloudsherpa.analytics.service.AnalyticsPersistenceService;
+import com.cloudsherpa.analytics.dto.MetricDTO;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,9 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.UUID;
-import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.List;
 
@@ -22,6 +22,9 @@ public class AnalyticsController
     // ! Currently only using 1 way of getting data (/metrics) because we only have 1 normalized_metrics table
     // ! We might want to change this such that the normalized_metrics table is split into 2 tables (1 for billing and 1 for usage)
 
+    // ! We also currently have no way of initializing the foreign key environment_id in the environment_reference table
+    // ! Will look into how we can do that
+
     private AnalyticsPersistenceService analyticsPersistenceService;
 
     public AnalyticsController(AnalyticsPersistenceService analyticsPersistenceService) 
@@ -29,20 +32,18 @@ public class AnalyticsController
         this.analyticsPersistenceService = analyticsPersistenceService;
     }
 
-    // POST request to inserta record into the database
+    // POST request to insert a record into the database
     // Insert into the AnalyticsDB
     @PostMapping("/metrics/record") // URL: /api/analytics/metrics/record
-    public String recordMetric(@RequestParam UUID environmentId, @RequestParam String resourceId,
-        @RequestParam String serviceCategory, @RequestParam BigDecimal usageAmount,
-        @RequestParam String usageUnit, @RequestParam BigDecimal costAmount, @RequestParam(defaultValue = "ZAR") String currency) 
+    public String recordMetric(@RequestBody MetricDTO request) 
     {
-        
-        analyticsPersistenceService.recordMetric(environmentId, resourceId, serviceCategory, usageAmount, usageUnit, costAmount, currency);
+        analyticsPersistenceService.recordMetric(request.getEnvironmentId(), request.getResourceId(), request.getServiceCategory(),
+            request.getUsageAmount(), request.getUsageUnit(), request.getCostAmount(), request.getCurrency()
+        );
         
         return "Metric recorded successfully";
     }
 
-    
     // GET request to retrieve data
     // Using List because this function will return multiple records
     // Spring automatically serializes the list into a JSON array
