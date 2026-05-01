8d70da9b202a9fc61404fedf330024c8a42e6753
FlipVenter <119102066+FlipVenter@users.noreply.github.com>
2026-05-01 22:48:08 +0200

    changed login page to use shadcn components and moved it's basic form to auth app/components/auth directory.

---
+++ apps/dashboard-frontend/app/authentication/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import LoginForm from "@/components/auth/login_form";
+
+
+export default function Authentication() {
+  return (
+    <LoginForm/>
+  );
+}
