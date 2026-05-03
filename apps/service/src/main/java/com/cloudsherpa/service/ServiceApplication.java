267e8dad063cb6fbf1f657266f192d285ea4943c
Star-shaker <norvalmegan05@gmail.com>
2026-05-04 00:58:50 +0200

    Added scheduling for service application and comments

--- apps/service/src/main/java/com/cloudsherpa/service/ServiceApplication.java
+++ apps/service/src/main/java/com/cloudsherpa/service/ServiceApplication.java
@@ -2,8 +2,13 @@ package com.cloudsherpa.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+
+// This annotation tells Spring to scan the codebase for methods annotated with @Scheduled 
+// (like PostgresNotificationListener) and execute them on background threads
+@EnableScheduling
 public class ServiceApplication {
 
 	public static void main(String[] args) {
