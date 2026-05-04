bd97d322df63a41f731dbed43ba2d33675faaefa
Star-shaker <norvalmegan05@gmail.com>
2026-05-04 20:00:27 +0200

    Added service listener

--- apps/service/src/main/java/com/cloudsherpa/service/ServiceApplication.java
+++ apps/service/src/main/java/com/cloudsherpa/service/ServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 
-// This annotation tells Spring to scan the codebase for methods annotated with @Scheduled 
-// (like PostgresNotificationListener) and execute them on background threads
+// This annotation tells Spring to scan the codebase for methods annotated with @Scheduled
 @EnableScheduling
 public class ServiceApplication {
 
