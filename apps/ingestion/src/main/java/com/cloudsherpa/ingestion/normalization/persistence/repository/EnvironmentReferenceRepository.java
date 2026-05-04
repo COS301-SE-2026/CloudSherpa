fc530c986f9da301a9e18b00b03ae09f5acdcb8d
Star-shaker <norvalmegan05@gmail.com>
2026-05-04 21:42:39 +0200

    Move entity and repository to ingestion

---
+++ apps/ingestion/src/main/java/com/cloudsherpa/ingestion/normalization/persistence/repository/EnvironmentReferenceRepository.java
@@ -0,0 +1,13 @@
+package com.cloudsherpa.ingestion.normalization.persistence.repository;
+
+import com.cloudsherpa.ingestion.normalization.persistence.entity.EnvironmentReference;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+// Needs to know which table it is managing and what the datatype is for its id field
+public interface EnvironmentReferenceRepository extends JpaRepository<EnvironmentReference, UUID> 
+{
+    // Interface instantly inherits dozens of pre-written database commands
+    // Can also add custom queries using method naming (optional)
+}
