7e13f09673c24fb14e818b7f0861c0242ede38f0
Star-shaker <norvalmegan05@gmail.com>
2026-05-06 21:37:21 +0200

    Cleaned CloudUsageController of mocks

--- apps/ingestion/src/main/java/com/cloudsherpa/ingestion/controller/CloudUsageController.java
+++ apps/ingestion/src/main/java/com/cloudsherpa/ingestion/controller/CloudUsageController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cloudsherpa.ingestion.models.IngestionRequestEvent;
 import com.cloudsherpa.ingestion.models.IngestionResult;
-import com.cloudsherpa.ingestion.normalization.model.NormalizedMetric;
 import com.cloudsherpa.ingestion.service.CloudUsageService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,22 +22,5 @@ public class CloudUsageController {
     @PostMapping("/ingest")
     public IngestionResult ingest(@RequestBody IngestionRequestEvent request) {
         return cloudUsageService.ingest(request);
-    }
-
-    // POST even though no request body, produce artifact (metric) => POST
-    @PostMapping("/mock")
-    public NormalizedMetric sendMockEvent() {
-        return cloudUsageService.sendMockEvent();
-    }
-
-    // Maps HTTP POST requests sent to "/api/events/flow/trigger" to this method.
-    // It's a POST because it initiates an action that modifies state (processing
-    // data).
-    @PostMapping("/flow/trigger")
-    public String triggerDataFlow() {
-        // Calls service to begin the entire ingestion pipeline.
-        cloudUsageService.ingestAndProcessMetrics();
-
-        return "Data flow triggered.";
     }
 }
