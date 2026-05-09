0e1a889fe00f2dbf54a10da0029dbb15939967ff
CheriseHeyl <cheriseheyl@gmail.com>
2026-05-09 18:38:54 +0200

    Added ability to ingest multiple accounts, services and metrics in one request

--- apps/ingestion/src/main/java/com/cloudsherpa/ingestion/provider/aws/AwsCloudConnector.java
+++ apps/ingestion/src/main/java/com/cloudsherpa/ingestion/provider/aws/AwsCloudConnector.java
@@ -1,54 +1,94 @@
 package com.cloudsherpa.ingestion.provider.aws;
 
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
 import com.cloudsherpa.ingestion.connector.*;
 import com.cloudsherpa.ingestion.models.*;
 
+import java.time.Instant;
+import java.util.*;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cloudwatch.CloudWatchClient;
 import software.amazon.awssdk.services.cloudwatch.model.*;
-
-import java.time.Instant;
-import java.util.*;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.DescribeInstancesRequest;
+import software.amazon.awssdk.services.ec2.model.DescribeInstancesResponse;
+import software.amazon.awssdk.services.ec2.model.Reservation;
 
 @Component("AWS")
 public class AwsCloudConnector implements CloudConnector, UsageCapable, BillingCapable {
 
   private final CloudWatchClient client = CloudWatchClient.builder()
+      .credentialsProvider(DefaultCredentialsProvider.create())
       .region(Region.AF_SOUTH_1)
       .build();
 
-  @Override
-  public List<UsageRecordModel> fetchUsage(AccountScope scope, IngestionRequestEvent request) {
+  public List<String> getAllInstanceIds(Ec2Client ec2) {
+    List<String> instanceIds = new ArrayList<>();
 
-    GetMetricStatisticsRequest req = GetMetricStatisticsRequest.builder()
-        .namespace("AWS/EC2")
-        .metricName("CPUUtilization")
-        .startTime(request.getFrom())
-        .endTime(request.getTo())
-        .period(300)
-        .statistics(Statistic.AVERAGE)
+    DescribeInstancesRequest request = DescribeInstancesRequest.builder().build();
+
+    for (DescribeInstancesResponse page : ec2.describeInstancesPaginator(request)) {
+      for (Reservation reservation : page.reservations()) {
+        for (software.amazon.awssdk.services.ec2.model.Instance instance : reservation.instances()) {
+          instanceIds.add(instance.instanceId());
+        }
+      }
+    }
+
+    return instanceIds;
+  }
+
+  @Override
+  public List<UsageRecordModel> fetchUsage(IngestionRequestEvent request) {
+    DefaultCredentialsProvider.create();
+    Ec2Client ec2 = Ec2Client.builder()
+        .region(Region.AF_SOUTH_1)
+        .credentialsProvider(DefaultCredentialsProvider.create())
         .build();
 
+    List<String> instanceIds = getAllInstanceIds(ec2);
+
     List<UsageRecordModel> result = new ArrayList<>();
+    for (AccountScope accScope : request.getScopes()) { // one user may have multiple aws accounts, these can be
+                                                        // monitored with one request
+      for (ServiceScope serviceScope : accScope.getServiceScopes()) { // these are for services such as EC2, RDS etc.
 
-    for (Datapoint dp : client.getMetricStatistics(req).datapoints()) {
+        for (String instanceId : instanceIds) { // instances within a service e.g. i-23xxxxxxx
+          Dimension dimension = Dimension.builder().name("InstanceId").value(instanceId).build();
 
-      UsageRecordModel r = new UsageRecordModel();
-      r.setProvider("AWS");
-      r.setAccountId(scope.getAccountId());
-      r.setServiceName("EC2");
-      r.setMetricName("CPUUtilization");
-      r.setValue(dp.average());
-      r.setUnit(dp.unit().name());
-      r.setTimestamp(dp.timestamp());
-      r.setIngestionTimestamp(Instant.now());
-      r.setRecordId(UUID.randomUUID());
+          for (String metric : serviceScope.getMetrics()) { // the metrics requested, e.g. CPUUtilisation, NetworkIn,
+                                                            // NetworkOut etc.
+            GetMetricStatisticsRequest req = GetMetricStatisticsRequest.builder()
+                .namespace(serviceScope.getName())
+                .metricName(metric)
+                .startTime(request.getFrom())
+                .endTime(request.getTo())
+                .period(600)
+                .dimensions(dimension)
+                .statistics(Statistic.AVERAGE)
+                .build();
 
-      result.add(r);
+            for (Datapoint dp : client.getMetricStatistics(req).datapoints()) {
+
+              UsageRecordModel r = new UsageRecordModel();
+              r.setProvider(accScope.getProvider());
+              r.setAccountId(accScope.getAccountId());
+              r.setServiceName(serviceScope.getName());
+              r.setMetricName(metric);
+              r.setValue(dp.average());
+              r.setUnit(dp.unit().name());
+              r.setTimestamp(dp.timestamp());
+              r.setIngestionTimestamp(Instant.now());
+              r.setRecordId(UUID.randomUUID());
+
+              result.add(r);
+            }
+          }
+        }
+      }
     }
 
     return result;
