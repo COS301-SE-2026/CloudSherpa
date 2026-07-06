c915c76b03601171fa7512c8c1c5670923787ce0
CheriseHeyl <cheriseheyl@gmail.com>
2026-07-06 23:36:03 +0200

    Added createAccount method

--- apps/service/src/main/java/com/cloudsherpa/service/persistconnection/aws/service/AwsConnectionPersistenceService.java
+++ apps/service/src/main/java/com/cloudsherpa/service/persistconnection/aws/service/AwsConnectionPersistenceService.java
@@ -1,6 +1,8 @@
 package com.cloudsherpa.service.persistconnection.aws.service;
 
+import com.cloudsherpa.lib.entities.CloudAccount;
 import com.cloudsherpa.lib.entities.CloudConnection;
+import com.cloudsherpa.lib.repositories.CloudAccountRepository;
 import com.cloudsherpa.lib.repositories.CloudConnectionRepository;
 import com.cloudsherpa.service.persistconnection.aws.dto.PersistAwsConnectionRequest;
 import java.time.Instant;
@@ -13,14 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class AwsConnectionPersistenceService {
   private final CloudConnectionRepository cloudConnectionRepository;
+  private final CloudAccountRepository cloudAccountRepository;
 
-  public AwsConnectionPersistenceService(CloudConnectionRepository cloudConnectionRepository) {
+  public AwsConnectionPersistenceService(
+      CloudConnectionRepository cloudConnectionRepository,
+      CloudAccountRepository cloudAccountRepository) {
     this.cloudConnectionRepository = cloudConnectionRepository;
+    this.cloudAccountRepository = cloudAccountRepository;
   }
 
   @Transactional
   public void persistConnection(PersistAwsConnectionRequest request) {
     CloudConnection connection = getOrCreateConnection(request);
+    CloudAccount account = createAccount(connection, request);
   }
 
   private CloudConnection getOrCreateConnection(PersistAwsConnectionRequest request) {
@@ -40,5 +47,20 @@ public class AwsConnectionPersistenceService {
       return cloudConnectionRepository.save(connection);
     }
     return optionalConnection.getFirst();
+  }
+
+  private CloudAccount createAccount(
+      CloudConnection connection, PersistAwsConnectionRequest request) {
+
+    CloudAccount account =
+        new CloudAccount(
+            UUID.randomUUID(),
+            connection.getId(),
+            "User",
+            request.displayName(),
+            request.ingestionPeriod().toString(),
+            Instant.now().atOffset(ZoneOffset.of("SAST")));
+
+    return cloudAccountRepository.save(account);
   }
 }
