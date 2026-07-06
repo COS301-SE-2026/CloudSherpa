83ed9d728addc6f480204fe193a63a0d13188789
CheriseHeyl <cheriseheyl@gmail.com>
2026-07-06 22:51:07 +0200

    Added PersistenceService with getOrCreateConnection method

---
+++ apps/service/src/main/java/com/cloudsherpa/service/persistconnection/aws/service/AwsConnectionPersistenceService.java
@@ -0,0 +1,44 @@
+package com.cloudsherpa.service.persistconnection.aws.service;
+
+import com.cloudsherpa.lib.entities.CloudConnection;
+import com.cloudsherpa.lib.repositories.CloudConnectionRepository;
+import com.cloudsherpa.service.persistconnection.aws.dto.PersistAwsConnectionRequest;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AwsConnectionPersistenceService {
+  private final CloudConnectionRepository cloudConnectionRepository;
+
+  public AwsConnectionPersistenceService(CloudConnectionRepository cloudConnectionRepository) {
+    this.cloudConnectionRepository = cloudConnectionRepository;
+  }
+
+  @Transactional
+  public void persistConnection(PersistAwsConnectionRequest request) {
+    CloudConnection connection = getOrCreateConnection(request);
+  }
+
+  private CloudConnection getOrCreateConnection(PersistAwsConnectionRequest request) {
+
+    List<CloudConnection> optionalConnection =
+        cloudConnectionRepository.findByUserIdAndProvider(request.userId(), "AWS");
+
+    if (optionalConnection.isEmpty()) {
+      UUID connectionId = UUID.randomUUID();
+      CloudConnection connection =
+          new CloudConnection(
+              connectionId,
+              request.userId(),
+              "AWS",
+              "Active",
+              Instant.now().atOffset(ZoneOffset.of("SAST")));
+      return cloudConnectionRepository.save(connection);
+    }
+    return optionalConnection.getFirst();
+  }
+}
