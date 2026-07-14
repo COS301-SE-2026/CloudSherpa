141f5d1b6263b7906a317ce519d36e84ca8fad29
g3rard-j <jordaangerard10@gmail.com>
2026-07-14 15:15:04 +0200

    KPI form details & init resources section

--- apps/dashboard/src/app/(app)/edit/kpi/page.tsx
+++ apps/dashboard/src/app/(app)/edit/kpi/page.tsx
@@ -1,7 +1,35 @@
 import { Button } from "@/components/atoms/button";
 import { Card, CardTitle } from "@/components/atoms/card";
+import {
+    Field,
+    FieldDescription,
+    FieldGroup,
+    FieldLabel,
+    FieldLegend,
+    FieldSeparator,
+    FieldSet,
+} from "@/components/atoms/field";
+import { FormCountCircle } from "@/components/atoms/form-count-circle";
+import { Input } from "@/components/atoms/input";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/atoms/input-group";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/atoms/select";
+import { SearchIcon } from "lucide-react";
 
 export default function EditKpiPage() {
+    const mockConnections = [
+        { label: "All connections", value: "all" },
+        { label: "Connection-1", value: "connection-1" },
+        { label: "Connection-2", value: "connection-2" },
+        { label: "Connection-3", value: "connection-3" },
+    ];
+
     return (
         <main className="flex flex-1 flex-col gap-6 p-6 lg:p-8 w-full mx-auto">
             <div className="flex flex-row gap-6">
@@ -10,7 +38,67 @@ export default function EditKpiPage() {
                 <Button variant={"secondary"}>Cancel</Button>
             </div>
             <div className="grid grid-cols-[2fr_1fr] gap-4 h-full">
-                <Card className="p-6"></Card>
+                <Card className="p-6">
+                    <FieldSet>
+                        <div className="flex flex-row items-center gap-3">
+                            <FormCountCircle count={1} />
+                            <FieldLegend className="mb-0">KPI Details</FieldLegend>
+                        </div>
+                        <FieldDescription>
+                            Choose the title that will appear on the dashboard card.
+                        </FieldDescription>
+                        <FieldGroup>
+                            <Field>
+                                <FieldLabel>Card Title</FieldLabel>
+                                <Input placeholder="Card Title"></Input>
+                            </Field>
+                        </FieldGroup>
+                    </FieldSet>
+                    <FieldSeparator></FieldSeparator>
+                    <FieldSet>
+                        <div className="flex flex-row items-center gap-3">
+                            <FormCountCircle count={2} />
+                            <FieldLegend className="mb-0">Resources</FieldLegend>
+                        </div>
+                        <FieldDescription>
+                            Select the resources whose costs should be aggregated.
+                        </FieldDescription>
+                        <div className="grid grid-cols-[1fr_2fr] gap-6">
+                            <div>
+                                <FieldLabel>Connection</FieldLabel>
+                                <Select defaultValue="all">
+                                    <SelectTrigger className="w-full">
+                                        <SelectValue />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectGroup>
+                                            {mockConnections.map((connection) => (
+                                                <SelectItem
+                                                    key={connection.value}
+                                                    value={connection.value}
+                                                >
+                                                    {connection.label}
+                                                </SelectItem>
+                                            ))}
+                                        </SelectGroup>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                            <div>
+                                <FieldLabel>Search Resources</FieldLabel>
+                                <InputGroup className="w-80">
+                                    <InputGroupInput placeholder="Search by resource name or ID" />
+                                    <InputGroupAddon>
+                                        <SearchIcon />
+                                    </InputGroupAddon>
+                                </InputGroup>
+                            </div>
+                        </div>
+                    </FieldSet>
+
+                    <FieldSeparator />
+                </Card>
+
                 <Card className="p-6">
                     <CardTitle>Preview</CardTitle>
                 </Card>
