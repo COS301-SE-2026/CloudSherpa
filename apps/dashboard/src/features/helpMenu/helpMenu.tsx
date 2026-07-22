b222bc8d41624159a4b3b46bf9673759f13edbda
Karishma <u23538199@tuks.co.za>
2026-07-22 14:28:00 +0200

    added help links faqs and desc for it

--- apps/dashboard/src/features/helpMenu/helpMenu.tsx
+++ apps/dashboard/src/features/helpMenu/helpMenu.tsx
@@ -15,6 +15,84 @@ import {Dialog, DialogHeader, DialogContent, DialogTitle} from "@/components/ato
 -
 */
 
+interface LinksForHelp{
+  id : string;
+  label : string;
+  description : string;
+  icon : ComponentType<{className?: string; strokeWidth?: number}>;
+  href? : string;
+  action? : "shortcut" | "first time user";
+}
+
+const LINKS : LinksForHelp[] = [
+  { id : "help center", label : "Help Center",
+    description : "Browse documents and guides", icon : BookOpen,
+    href : "documentsAndTutorials",
+  },
+
+  { id : "tutorials", label : "Tutorials",
+    description : "Step-by-step CloudSherpa walkthroughs", icon : PlayCircle,
+    href : "/documentsAndTutorials",
+  },
+
+  { id : "shortcut", label : "Keyboard Shortcuts",
+    description : "Navigate CloudSherpa faster", icon : Command,
+    action : "shortcut",
+  },
+
+  { id : "first time user", label : "First time user",
+    description : "Learn by watching", icon : MessageCircle,
+    action : "first time user",
+  },
+];
+
+interface FaQuestion{
+  id : string;
+  question : string;
+  answer : string;
+}
+
+const QUESTION : FaQuestion[] = [
+  {
+    id : "question1",
+    question : "How do I connect my cloud provider?",
+    answer : "Go to Connection Manager and click on Add. Follow the steps of the wizard by entering your credentials and selecting your services and resource to be monitored. You have then successfully connected your cloud provider!",
+  },
+
+  {
+    id : "question2",
+    question : "How do I connect my cloud provider?",
+    answer : "Go to Connection Manager and click on Add. Follow the steps of the wizard by entering your credentials and selecting your services and resource to be monitored. You have then successfully connected your cloud provider!",
+  },
+
+  {
+    id : "question3",
+    question : "How do I connect my cloud provider?",
+    answer : "Go to Connection Manager and click on Add. Follow the steps of the wizard by entering your credentials and selecting your services and resource to be monitored. You have then successfully connected your cloud provider!",
+  },
+
+  {
+    id : "question4",
+    question : "How do I connect my cloud provider?",
+    answer : "Go to Connection Manager and click on Add. Follow the steps of the wizard by entering your credentials and selecting your services and resource to be monitored. You have then successfully connected your cloud provider!",
+  },
+
+  {
+    id : "question5",
+    question : "How do I connect my cloud provider?",
+    answer : "Go to Connection Manager and click on Add. Follow the steps of the wizard by entering your credentials and selecting your services and resource to be monitored. You have then successfully connected your cloud provider!",
+  },
+];
+
+interface KeyboardShortcuts{
+  key : string[];
+  function : string;
+}
+
+const SHORTCUT : KeyboardShortcuts[] = [
+  {key : ["ENTER"], function : "Submit form"},
+];
+
 export function HelpMenu(){
   const [open, setOpen] = useState(false);
 
