88a602094e78cc1388fba47b49acc3b4965dca41
Karishma <u23538199@tuks.co.za>
2026-07-24 12:24:00 +0200

    added it to the app wrapper

--- apps/dashboard/src/app/(app)/layout.tsx
+++ apps/dashboard/src/app/(app)/layout.tsx
@@ -2,15 +2,24 @@ import { SidebarProvider, SidebarInset } from "@/components/atoms/sidebar";
 import { TooltipProvider } from "@/components/atoms/tooltip";
 import { AppSidebar } from "@/components/molecules/app-sidebar";
 
+//added these for the onboarding
+import {NextStepProvider, NextStep} from "nextstepjs";
+import {dashboardTour} from "@/features/onboarding/dashboardTour";
+import tourCard from "@/features/onboarding/tourCard";
+
 export default function AppWrapper({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <TooltipProvider>
-            <SidebarProvider>
-                <AppSidebar />
-                <SidebarInset className="flex flex-col flex-1 overflow-hidden">
-                    <div className="flex flex-1 flex-col gap-4">{children}</div>
-                </SidebarInset>
-            </SidebarProvider>
-        </TooltipProvider>
+        <NextStepProvider>
+            <NextStep steps = {dashboardTour} cardComponent = {tourCard}>
+                <TooltipProvider>
+                    <SidebarProvider>
+                        <AppSidebar />
+                        <SidebarInset className="flex flex-col flex-1 overflow-hidden">
+                            <div className="flex flex-1 flex-col gap-4">{children}</div>
+                        </SidebarInset>
+                    </SidebarProvider>
+                </TooltipProvider>
+            </NextStep>
+        </NextStepProvider>
     );
 }
