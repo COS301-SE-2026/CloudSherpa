2d056313d68276ff436619735c526d5427c6b59d
Karishma <u23538199@tuks.co.za>
2026-07-27 00:26:30 +0200

    attempt at reducing duplicate code for connections and resources

--- apps/dashboard/src/features/helpMenu/documents/connections.tsx
+++ apps/dashboard/src/features/helpMenu/documents/connections.tsx
@@ -2,73 +2,62 @@
 
 import { Plug } from "lucide-react";
 import HelpCenter from "@/features/helpMenu/documents/documentsPage";
+import {creatingIns} from "@/features/helpMenu/documents/createInstructions";
 
 /*
 - this page give users all the info on how to add, remove and manage their conn
 */
 
-interface Instructions {
-    id: string;
-    name: string;
-    description: string;
-    details: string[];
-}
+export default function Connection(){
+    const INS = creatingIns([
+        {
+            name: "Navigate to Connection Manager",
+            description: "Access the Connection Manager from your dashboard",
+            details: [
+                "From your dashboard, locate the sidebar on the left",
+                "Click on Connection Manager",
+                "You will be redirected to the Connection Manager page where you can view and configure your cloud connections",
+            ],
+        },
 
-const INS: Instructions[] = [
-    {
-        id: "ins1",
-        name: "Navigate to Connection Manager",
-        description: "Access the Connection Manager from your dashboard",
-        details: [
-            "From your dashboard, locate the sidebar on the left",
-            "Click on Connection Manager",
-            "You will be redirected to the Connection Manager page where you can view and configure your cloud connections",
-        ],
-    },
+        {
+            name: "Add an AWS cloud provider",
+            description: "Follow the steps of the wizard to add your first cloud provider",
+            details: [
+                "On the Connection Manager page, click on add button located in the top right corner",
+                "A wizard will appear",
+                "Follow the steps of the wizard and you will be able to successfully add your first AWS connection",
+            ],
+        },
 
-    {
-        id: "ins2",
-        name: "Add an AWS cloud provider",
-        description: "Follow the steps of the wizard to add your first cloud provider",
-        details: [
-            "On the Connection Manager page, click on add button located in the top right corner",
-            "A wizard will appear",
-            "Follow the steps of the wizard and you will be able to successfully add your first AWS connection",
-        ],
-    },
+        {
+            name: "Enter your credentials",
+            description: "Provide your credentials for the cloud provider",
+            details: [
+                "Enter your cloud providers credentials",
+                "Ensure you have the required permissions",
+            ],
+        },
 
-    {
-        id: "ins3",
-        name: "Enter your credentials",
-        description: "Provide your credentials for the cloud provider",
-        details: [
-            "Enter your cloud providers credentials",
-            "Ensure you have the required permissions",
-        ],
-    },
+        {
+            name: "Select services and resources",
+            description: "Choose which services and resources you would like CloudSherpa to monitor",
+            details: [
+                "Select the services you want to keep track of",
+                "Choose the specific resources within each service you want to monitor",
+            ],
+        },
 
-    {
-        id: "ins4",
-        name: "Select services and resources",
-        description: "Choose which services and resources you would like CloudSherpa to monitor",
-        details: [
-            "Select the services you want to keep track of",
-            "Choose the specific resources within each service you want to monitor",
-        ],
-    },
+        {
+            name: "Manage your connections",
+            description: "View and configure your connections",
+            details: [
+                "You can add or remove connections at any time from the Connection Manager",
+                "You are able to rename your connections",
+            ],
+        },
+    ]);
 
-    {
-        id: "ins5",
-        name: "Manage your connections",
-        description: "View and configure your connections",
-        details: [
-            "You can add or remove connections at any time from the Connection Manager",
-            "You are able to rename your connections",
-        ],
-    },
-];
-
-export default function Connection() {
     return (
         <HelpCenter name = "Connecting your AWS cloud provider"
                     description = "Follow these five steps to connect your AWS account"
@@ -77,4 +66,5 @@ export default function Connection() {
                     instructions = {INS}
         />
     );
+
 }
--- apps/dashboard/src/features/helpMenu/documents/resources.tsx
+++ apps/dashboard/src/features/helpMenu/documents/resources.tsx
@@ -2,51 +2,42 @@
 
 import { Boxes } from "lucide-react";
 import HelpCenter from "@/features/helpMenu/documents/documentsPage";
+import {creatingIns} from "@/features/helpMenu/documents/createInstructions";
 
 /*
 - users should be able to easily follow the ins on how to get to, manage and delete theri resources
 */
 
-interface Instructions {
-    id: string;
-    name: string;
-    description: string;
-    details: string[];
-}
+export default function Resource(){
+    const INS = creatingIns([
+        {
+            name: "Access the Resource Manager",
+            description: "Navigate to the Resource Manager from your dashboard",
+            details: [
+                "From your dashboard, locate the sidebar on the left",
+                "Select Resource Manager",
+                "Once redirected you will be able to view all your active and inactive resources associated with a specific service",
+            ],
+        },
 
-const INS: Instructions[] = [
-    {
-        id: "ins1",
-        name: "Access the Resource Manager",
-        description: "Navigate to the Resource Manager from your dashboard",
-        details: [
-            "From your dashboard, locate the sidebar on the left",
-            "Select Resource Manager",
-            "Once redirected you will be able to view all your active and inactive resources associated with a specific service",
-        ],
-    },
+        {
+            name: "Filter and search for resources",
+            description: "Use filters and search to find a resource",
+            details: [
+                "Use the search bar to find a particular resource",
+                "Filter the table to view the resources",
+            ],
+        },
 
-    {
-        id: "ins2",
-        name: "Filter and search for resources",
-        description: "Use filters and search to find a resource",
-        details: [
-            "Use the search bar to find a particular resource",
-            "Filter the table to view the resources",
-        ],
-    },
+        {
+            name: "Active or Inactive resources",
+            description: "Manage which resources are being monitored",
+            details: [
+                "Toggle on a resource to select which resource should be monitored (active) and which should not (inactive)",
+            ],
+        },
+    ]);
 
-    {
-        id: "ins3",
-        name: "Active or Inactive resources",
-        description: "Manage which resources are being monitored",
-        details: [
-            "Toggle on a resource to select which resource should be monitored (active) and which should not (inactive)",
-        ],
-    },
-];
-
-export default function Resources() {
     return (
         <HelpCenter name = "Managing your cloud resources"
                     description = "Learn how to manage and configure your cloud resources effectively with CloudSherpa"
@@ -55,4 +46,5 @@ export default function Resources() {
                     instructions = {INS}
         />
     );
+
 }
