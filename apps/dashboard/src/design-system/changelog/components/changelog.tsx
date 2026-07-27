99dd0b58ad3dc46ac90a4c15b0627bfdcff2d7ef
FlipVenter <flippiefliptone@gmail.com>
2026-07-27 13:02:35 +0200

    added logo section to changelog in designsystem

--- apps/dashboard/src/design-system/changelog/components/changelog.tsx
+++ apps/dashboard/src/design-system/changelog/components/changelog.tsx
@@ -1,9 +1,11 @@
 import ColourChanges from "@/design-system/changelog/components/colourChanges";
+import LogoChanges from "./logoChanges";
 
 export default function Changelog() {
     return (
         <div className="space-y-6">
             <ColourChanges />
+            <LogoChanges />
         </div>
     );
 }
