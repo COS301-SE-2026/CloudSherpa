2681fa8445bfd09800eedc72f6a33ff0c00699e8
Karishma <u23538199@tuks.co.za>
2026-07-30 16:23:10 +0200

    trying to fix error

--- apps/dashboard/src/features/landingPage/components/landingPage.tsx
+++ apps/dashboard/src/features/landingPage/components/landingPage.tsx
@@ -7,11 +7,22 @@ import { Solution } from "./solution";
 import { HowItWorks } from "./howItWorks";
 import { WhoItsFor } from "./whoItsFor";
 import { Features } from "./featureCards";
-import AuthAnimation from "@/features/authentication/components/authanimation";
+import dynamic from "next/dynamic";
 
 /*
 - combines all ages
 */
+
+//used a similar approach as the login & register, skips server rendering and defers mounting authani until after the client loads
+//will show the div as a placeholder in the meantime
+const AuthAnimation = dynamic(() => import("@/features/authentication/components/authanimation"),{
+    ssr : false,
+
+    loading : () => (
+        <div className = "absolute top-1/2 left-0 w-full h-[60%] -translate-y-1/2 bg-transparent"/>
+    ),
+
+});
 
 export default function LandingPage() {
     const [scrolled, setScrolled] = useState(false);
