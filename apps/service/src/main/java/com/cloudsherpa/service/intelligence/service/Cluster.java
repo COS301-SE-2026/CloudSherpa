c5776d75cdd7878ef1b3ac5b57d142d8bf69fccc
g3rard-j <jordaangerard10@gmail.com>
2026-08-07 14:45:09 +0200

    Resolve sonar issue regarding visibility of clusterValues

--- apps/service/src/main/java/com/cloudsherpa/service/intelligence/service/Cluster.java
+++ apps/service/src/main/java/com/cloudsherpa/service/intelligence/service/Cluster.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cluster {
-  public List<Long> clusterValues;
+  private List<Long> clusterValues;
 
   public Cluster() {
     clusterValues = new ArrayList<>();
