f8f20a832d64678c1e8080400ffa8683d98ae888
g3rard-j <jordaangerard10@gmail.com>
2026-08-14 14:42:45 +0200

    Forecast and historical billing variance calculations

--- apps/service/src/main/java/com/cloudsherpa/service/intelligence/service/billing/BillingAnalyticsService.java
+++ apps/service/src/main/java/com/cloudsherpa/service/intelligence/service/billing/BillingAnalyticsService.java
@@ -2,6 +2,9 @@ package com.cloudsherpa.service.intelligence.service.billing;
 
 import com.cloudsherpa.lib.repositories.NormalizedCostsRepository;
 import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.Map;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +12,8 @@ import org.springframework.stereotype.Service;
 public class BillingAnalyticsService {
 
   private final NormalizedCostsRepository normalizedCostsRepository;
+
+  private static final Integer DAY_IN_SECONDS = 86400;
 
   public BillingAnalyticsService(NormalizedCostsRepository normalizedCostsRepository) {
     this.normalizedCostsRepository = normalizedCostsRepository;
@@ -41,8 +46,28 @@ public class BillingAnalyticsService {
     return chargeLabel.toString();
   }
 
-  private BigDecimal variance() {
-    return null;
+  private BigDecimal variance(
+      Instant firstForecastDate, Integer forecastSteps, BigDecimal cumalativeForecast) {
+
+    // Assumptions:
+    // Forecast remain at daily granularity
+
+    Instant to = firstForecastDate.minusSeconds(DAY_IN_SECONDS);
+    // cast to long to prevent int overflow
+    Instant from = to.minusSeconds((long) DAY_IN_SECONDS * forecastSteps);
+
+    BigDecimal pastCost =
+        normalizedCostsRepository.sumTotalCostBetween(
+            OffsetDateTime.ofInstant(from, ZoneOffset.UTC),
+            OffsetDateTime.ofInstant(to, ZoneOffset.UTC));
+
+    if (cumalativeForecast.equals(pastCost)) {
+      return BigDecimal.ZERO;
+    }
+
+    BigDecimal absoluteDifference = cumalativeForecast.subtract(pastCost).abs();
+    BigDecimal average = cumalativeForecast.divide(absoluteDifference);
+    return absoluteDifference.divide(average).multiply(BigDecimal.valueOf(100.0));
   }
 
   private String primaryCostDriver(Map<String, BigDecimal> individualChargeForecastResults) {
