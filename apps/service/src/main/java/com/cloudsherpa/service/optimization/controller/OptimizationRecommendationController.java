dbd8d52b7c72ee9aa78762907ee81eb97e350904
Star-shaker <norvalmegan05@gmail.com>
2026-08-15 15:44:55 +0200

    Move to controller directory

--- apps/service/src/main/java/com/cloudsherpa/service/optimization/controller/OptimizationRecommendationController.java
+++ apps/service/src/main/java/com/cloudsherpa/service/optimization/controller/OptimizationRecommendationController.java
@@ -1,4 +1,4 @@
-package com.cloudsherpa.service.optimization;
+package com.cloudsherpa.service.optimization.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
