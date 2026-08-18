c086b10331c8656334fdb32e9e3463177e36f86a
Star-shaker <norvalmegan05@gmail.com>
2026-08-18 19:04:29 +0200

    Add scheduler init

--- apps/service/src/main/java/com/cloudsherpa/service/optimization/worker/OptimizationWorker.java
+++ apps/service/src/main/java/com/cloudsherpa/service/optimization/worker/OptimizationWorker.java
@@ -1,3 +1,25 @@
 package com.cloudsherpa.service.optimization.worker;
 
-public class OptimizationWorker {}
+import com.cloudsherpa.service.optimization.service.TenantService;
+import java.util.UUID;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OptimizationWorker {
+
+  private final TenantService tenantService;
+
+  public OptimizationWorker(TenantService tenantService) {
+    this.tenantService = tenantService;
+  }
+
+  @Scheduled(fixedRate = 86400000) // once every 24 hours in ms
+  public void run() {
+    for (UUID tenantId : tenantService.findTenantIds()) {
+      processTenant(tenantId);
+    }
+  }
+
+  void processTenant(UUID tenantId) {}
+}
