4d42d0c112943a9b2f57033be262f97a4bc56bb0
g3rard-j <jordaangerard10@gmail.com>
2026-08-19 11:30:12 +0200

    Basic GCP billing persist normalized records

--- apps/ingestion/src/main/java/com/cloudsherpa/ingestion/billing/provider/gcp/bigquery/normalization/GcpBigQueryNormalizationService.java
+++ apps/ingestion/src/main/java/com/cloudsherpa/ingestion/billing/provider/gcp/bigquery/normalization/GcpBigQueryNormalizationService.java
@@ -1,11 +1,9 @@
 package com.cloudsherpa.ingestion.billing.provider.gcp.bigquery.normalization;
 
-import com.cloudsherpa.ingestion.billing.BillingExport;
 import com.cloudsherpa.ingestion.billing.provider.gcp.bigquery.pipeline.GcpBillingContext;
+import com.cloudsherpa.ingestion.service.SherpaDbPersistenceService;
 import com.cloudsherpa.lib.entities.NormalizedCosts;
 import com.google.cloud.bigquery.FieldValueList;
-import java.util.List;
-import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -14,24 +12,19 @@ import org.springframework.stereotype.Service;
 public class GcpBigQueryNormalizationService {
 
   private final Logger logger = LoggerFactory.getLogger(GcpBigQueryNormalizationService.class);
+  private final SherpaDbPersistenceService persistenceService;
+
+  public GcpBigQueryNormalizationService(SherpaDbPersistenceService persistenceService) {
+    this.persistenceService = persistenceService;
+  }
 
   public void normalize(GcpBillingContext context) {
     GcpBigQueryNormalizer gcpBigQueryNormalizer = new GcpBigQueryNormalizer();
     gcpBigQueryNormalizer.setBillingId(context.getBillingConfig().billingAccountId());
 
-    // Why is temp export necessary? A normalizedcost records containes a reference to the billing
-    // exports table,
-    // hence the related export object is normally passed from the context to be able to obtain the
-    // necessary data
-    // needed to construct the normalizedcost record. Since the BillingExport is currently a no-go
-    // for GCP, this
-    // temp export is created to be able to test normalization logic even though the database does
-    // not yet support it
-    BillingExport tempExport =
-        new BillingExport(UUID.randomUUID().toString(), UUID.randomUUID().toString(), List.of());
-
     for (FieldValueList fieldValueList : context.getTableResult().getValues()) {
-      NormalizedCosts normalizedCosts = gcpBigQueryNormalizer.normalize(fieldValueList, tempExport);
+      NormalizedCosts normalizedCosts =
+          gcpBigQueryNormalizer.normalize(fieldValueList, context.getBillingExport());
       logger.info(
           "Normalized GCP BigQuery cost record: costId={}, executionId={}, resourceId={}, chargeId={}, provider={}, billingAccountId={}, serviceName={}, chargeType={}, costAmount={}, currency={}, usageStartTime={}, usageEndTime={}, metadata={}",
           normalizedCosts.getCostId(),
@@ -47,6 +40,7 @@ public class GcpBigQueryNormalizationService {
           normalizedCosts.getUsageStartTime(),
           normalizedCosts.getUsageEndTime(),
           normalizedCosts.getMetadata());
+      persistenceService.recordCost(normalizedCosts, context.getUserId());
     }
   }
 }
