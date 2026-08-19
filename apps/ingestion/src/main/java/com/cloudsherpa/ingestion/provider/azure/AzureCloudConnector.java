e0f4de18712699796ae5265cb0848d10d276aef1
CheriseHeyl <cheriseheyl@gmail.com>
2026-08-20 01:03:13 +0200

    Added azure cloud connector

---
+++ apps/ingestion/src/main/java/com/cloudsherpa/ingestion/provider/azure/AzureCloudConnector.java
@@ -0,0 +1,73 @@
+package com.cloudsherpa.ingestion.provider.azure;
+
+import com.cloudsherpa.ingestion.connector.AccountScope;
+import com.cloudsherpa.ingestion.connector.BillingCapable;
+import com.cloudsherpa.ingestion.connector.CloudConnector;
+import com.cloudsherpa.ingestion.connector.CloudCredentials;
+import com.cloudsherpa.ingestion.connector.UsageCapable;
+import com.cloudsherpa.ingestion.models.BillingRecordModel;
+import com.cloudsherpa.ingestion.models.IngestionRequestEvent;
+import com.cloudsherpa.ingestion.models.ResourceDetail;
+import com.cloudsherpa.ingestion.models.UsageRecordModel;
+import com.cloudsherpa.ingestion.provider.aws.monitoring.AwsCloudWatchMetricProvider;
+import com.cloudsherpa.ingestion.provider.aws.monitoring.MockCloudWatchMetricProvider;
+import com.cloudsherpa.ingestion.provider.monitoring.CloudMonitoringMetricProvider;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component("azure")
+public class AzureCloudConnector implements CloudConnector, UsageCapable, BillingCapable {
+
+  private final CloudMonitoringMetricProvider metricProvider;
+  private final CloudMonitoringMetricProvider mockMetricProvider;
+
+  public AzureCloudConnector(MockCloudWatchMetricProvider mockMetricProvider) {
+    metricProvider = new AwsCloudWatchMetricProvider();
+    this.mockMetricProvider = mockMetricProvider;
+  }
+
+  @Override
+  public List<UsageRecordModel> fetchUsage(
+      AccountScope accountScope, IngestionRequestEvent request) {
+    return metricProvider.collectMetrics(accountScope, request);
+  }
+
+  @Override
+  public List<BillingRecordModel> fetchBilling(
+      AccountScope accountScope, IngestionRequestEvent request) {
+    return List.of(); // mock for now
+  }
+
+  @Override
+  public List<String> getAllOfferedServices() {
+    return List.of(); // mock for now
+  }
+
+  @Override
+  public List<ResourceDetail> getAllResources(
+      CloudCredentials credentials, List<String> serviceTypes) {
+    return List.of(); // mock for now
+  }
+
+  @Override
+  public List<BillingRecordModel> fetchMockBilling(
+      AccountScope accountScope, IngestionRequestEvent request) {
+    return List.of();
+  }
+
+  @Override
+  public boolean testConnection(CloudCredentials credentials) {
+    return true; // unimplemented
+  }
+
+  @Override
+  public String getProviderName() {
+    return "Azure";
+  }
+
+  @Override
+  public List<UsageRecordModel> fetchMockUsage(
+      AccountScope accountScope, IngestionRequestEvent request) {
+    return mockMetricProvider.collectMetrics(accountScope, request);
+  }
+}
