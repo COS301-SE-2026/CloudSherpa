f8918511b832696e52e4ea5a67bfe052917070ad
g3rard-j <jordaangerard10@gmail.com>
2026-08-20 08:58:22 +0200

    Init GCP setup wizard billing form

--- apps/dashboard/src/features/connectionManager/components/connectionManager/gcp/billingForm.tsx
+++ apps/dashboard/src/features/connectionManager/components/connectionManager/gcp/billingForm.tsx
@@ -1,4 +1,12 @@
+import {
+    Field,
+    FieldDescription,
+    FieldGroup,
+    FieldLabel,
+    FieldSet,
+} from "@/components/atoms/field";
 import { BillingFormContainer } from "../billingFormContainer";
+import { Input } from "@/components/atoms/input";
 
 interface GcpBillingFormProps {
     readonly optedInToBilling: boolean;
@@ -14,7 +22,29 @@ export function GcpBillingForm({
             optedInToBilling={optedInToBilling}
             handleOptedInToBillingChange={handleOptedInToBillingChange}
         >
-            test
+            <FieldSet>
+                <FieldGroup>
+                    <Field>
+                        <FieldLabel htmlFor="billingId">Cloud Billing Account ID</FieldLabel>
+                        <FieldDescription>
+                            Enter the Google Cloud Billing account ID for this connection.
+                        </FieldDescription>
+                        <Input
+                            id="billingId"
+                            type="text"
+                            placeholder="000000-000000-000000-000000"
+                        ></Input>
+                    </Field>
+                    <Field>
+                        <FieldLabel htmlFor="dataset">Billing Export Dataset ID</FieldLabel>
+                        <FieldDescription>
+                            Enter the BigQuery dataset that contains the detailed billing export
+                            tables.
+                        </FieldDescription>
+                        <Input id="dataset" type="text" placeholder="example_dataset"></Input>
+                    </Field>
+                </FieldGroup>
+            </FieldSet>
         </BillingFormContainer>
     );
 }
