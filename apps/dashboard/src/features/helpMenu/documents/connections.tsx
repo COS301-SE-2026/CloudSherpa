7a768f713cbbcfbc6615bf9aa2d4760397e2b855
Karishma <u23538199@tuks.co.za>
2026-08-29 15:05:39 +0200

    added instructions for gcp to the help menu

--- apps/dashboard/src/features/helpMenu/documents/connections.tsx
+++ apps/dashboard/src/features/helpMenu/documents/connections.tsx
@@ -8,56 +8,95 @@ import { creatingIns } from "@/features/helpMenu/documents/createInstructions";
 - this page give users all the info on how to add, remove and manage their conn
 */
 
+//removed from inside the function
+const Aws_INS = creatingIns([
+    {
+        name: "Navigate to Connection Manager",
+        description: "Access the Connection Manager from your dashboard",
+        details: [
+            "From your dashboard, locate the sidebar on the left",
+            "Click on Connection Manager",
+            "You will be redirected to the Connection Manager page where you can view and configure your cloud connections",
+        ],
+    },
+
+    {
+        name: "Add an AWS cloud provider",
+        description: "Follow the steps of the wizard to add your first cloud provider",
+        details: [
+            "On the Connection Manager page, click on add button located in the top right corner",
+            "A wizard will appear",
+            "Follow the steps of the wizard and you will be able to successfully add your first AWS connection",
+        ],
+    },
+
+    {
+        name: "Enter your credentials",
+        description: "Provide your credentials for the cloud provider",
+        details: [
+            "Enter your cloud providers credentials",
+            "Ensure you have the required permissions",
+        ],
+    },
+
+    {
+        name: "Select services and resources",
+        description:
+            "Choose which services and resources you would like CloudSherpa to monitor",
+        details: [
+            "Select the services you want to keep track of",
+            "Choose the specific resources within each service you want to monitor",
+        ],
+    },
+
+    {
+        name: "Manage your connections",
+        description: "View and configure your connections",
+        details: [
+            "You can add or remove connections at any time from the Connection Manager",
+            "You are able to rename your connections",
+        ],
+    },
+]);
+
+const Gcp_INS = creatingIns([
+    {name : "Navigate to Connection Manager",
+     description : "Access the Connection Manager from your dashboard",
+     details : [
+        "From your dashboard, locate the sidebar on the left",
+        "Click on Connection Manager",
+        "You will be redirected to the Connection Manager page where you can view and configure your cloud connections",
+     ],
+    },
+
+    {name : "Add a GCP cloud provider",
+     description : "Follow the steps of the wizard to add GCP as a cloud provider",
+     details : [
+        "On the Connection Manager page, click on the add button which is located on the top right hand side corner of the page",
+        "Follow the steps of the wizard to add you GCP connection",
+     ],
+    },
+
+    {name : "Enter your credentials",
+     description : "Provide your credentials for GCP",
+     details : [
+        "Add an account name which you can refer to when looking for the connection",
+        "Upload your GCP service account JSON file"
+     ],
+    },
+
+    {name : "Select your services and resources",
+     description : "Choose which services and resources you would like CloudSherpa to monitor",
+     details : [
+        "Select the services you would and you would then get a list of permissions that you would need to approve of and then proceed to step 3",
+        "You get all of the resources associated with that service and you can choose which ones you would like to monitor",
+        "The table can be filtered by the names of the resources or you can search for a resource using the toolbar",
+     ],
+    },
+]);
+
 export default function Connection() {
-    const INS = creatingIns([
-        {
-            name: "Navigate to Connection Manager",
-            description: "Access the Connection Manager from your dashboard",
-            details: [
-                "From your dashboard, locate the sidebar on the left",
-                "Click on Connection Manager",
-                "You will be redirected to the Connection Manager page where you can view and configure your cloud connections",
-            ],
-        },
-
-        {
-            name: "Add an AWS cloud provider",
-            description: "Follow the steps of the wizard to add your first cloud provider",
-            details: [
-                "On the Connection Manager page, click on add button located in the top right corner",
-                "A wizard will appear",
-                "Follow the steps of the wizard and you will be able to successfully add your first AWS connection",
-            ],
-        },
-
-        {
-            name: "Enter your credentials",
-            description: "Provide your credentials for the cloud provider",
-            details: [
-                "Enter your cloud providers credentials",
-                "Ensure you have the required permissions",
-            ],
-        },
-
-        {
-            name: "Select services and resources",
-            description:
-                "Choose which services and resources you would like CloudSherpa to monitor",
-            details: [
-                "Select the services you want to keep track of",
-                "Choose the specific resources within each service you want to monitor",
-            ],
-        },
-
-        {
-            name: "Manage your connections",
-            description: "View and configure your connections",
-            details: [
-                "You can add or remove connections at any time from the Connection Manager",
-                "You are able to rename your connections",
-            ],
-        },
-    ]);
+    
 
     return (
         <HelpCenter
