0b5e89cf14ec3fa0bfa73b4b673304cd019f8d30
g3rard-j <jordaangerard10@gmail.com>
2026-08-30 15:11:11 +0200

    Metric id autogeneration in seed script

--- nfr-tests/common/seed/historical-metric-seed.sql
+++ nfr-tests/common/seed/historical-metric-seed.sql
@@ -2,7 +2,6 @@
 
 CREATE OR REPLACE FUNCTION seed_metric_series(
     p_tenant_schema text,
-    p_metric_id uuid,
     p_resource_id uuid,
     p_metric_type text,
     p_metric_name text,
@@ -19,7 +18,6 @@ BEGIN
 
     EXECUTE format($sql$
         INSERT INTO %I.normalized_metrics (
-            metric_id,
             resource_id,
             recorded_at,
             metric_type,
@@ -31,21 +29,19 @@ BEGIN
             period_end
         )
         SELECT
-            $1 AS metric_id,
-            $2 AS resource_id,
+            $1 AS resource_id,
             NOW() AS recorded_at,
-            $3 AS metric_type,
-            $4 AS metric_name,
+            $2 AS metric_type,
+            $3 AS metric_name,
             i AS metric_value,
             'NFR unit' AS unit,
             'USD' AS currency,
-            $5 - (i * INTERVAL '5 min') AS period_start,
-            ($5 - (i * INTERVAL '5 min')) + INTERVAL '5 min' AS period_end
-        FROM generate_series(1, $6) AS series(i)
+            $4 - (i * INTERVAL '5 min') AS period_start,
+            ($4 - (i * INTERVAL '5 min')) + INTERVAL '5 min' AS period_end
+        FROM generate_series(1, $5) AS series(i)
         ON CONFLICT DO NOTHING
     $sql$, p_tenant_schema)
     USING
-        p_metric_id,
         p_resource_id,
         p_metric_type,
         p_metric_name,
@@ -78,34 +74,24 @@ DECLARE
     resource_id_09 uuid := '10000000-0000-0000-0000-000000000009';
     resource_id_10 uuid := '10000000-0000-0000-0000-000000000010';
 
-    metric_id_01 uuid := '20000000-0000-0000-0000-000000000001';
     metric_name_01 text := 'CPUUtilization';
     metric_type_01 text := 'cpu';
-    metric_id_02 uuid := '20000000-0000-0000-0000-000000000002';
     metric_name_02 text := 'NetworkIn';
     metric_type_02 text := 'network';
-    metric_id_03 uuid := '20000000-0000-0000-0000-000000000003';
     metric_name_03 text := 'NetworkOut';
     metric_type_03 text := 'network';
-    metric_id_04 uuid := '20000000-0000-0000-0000-000000000004';
     metric_name_04 text := 'DiskReadBytes';
     metric_type_04 text := 'disk';
-    metric_id_05 uuid := '20000000-0000-0000-0000-000000000005';
     metric_name_05 text := 'DiskWriteBytes';
     metric_type_05 text := 'disk';
-    metric_id_06 uuid := '20000000-0000-0000-0000-000000000006';
     metric_name_06 text := 'DatabaseConnections';
     metric_type_06 text := 'connections';
-    metric_id_07 uuid := '20000000-0000-0000-0000-000000000007';
     metric_name_07 text := 'ReadLatency';
     metric_type_07 text := 'latency';
-    metric_id_08 uuid := '20000000-0000-0000-0000-000000000008';
     metric_name_08 text := 'Invocations';
     metric_type_08 text := 'invocations';
-    metric_id_09 uuid := '20000000-0000-0000-0000-000000000009';
     metric_name_09 text := 'CPUUtilization';
     metric_type_09 text := 'cpu';
-    metric_id_10 uuid := '20000000-0000-0000-0000-000000000010';
     metric_name_10 text := 'CPUUtilization';
     metric_type_10 text := 'cpu';
 
@@ -160,43 +146,43 @@ BEGIN
     metric_series_datapoints := 10000;
 
     total_rows_seeded := total_rows_seeded + seed_metric_series(
-        tenant_schema, metric_id_01, resource_id_01, metric_type_01, metric_name_01,
+        tenant_schema, resource_id_01, metric_type_01, metric_name_01,
         to_timestamp, metric_series_datapoints
     );
     total_rows_seeded := total_rows_seeded + seed_metric_series(
-        tenant_schema, metric_id_02, resource_id_02, metric_type_02, metric_name_02,
+        tenant_schema, resource_id_02, metric_type_02, metric_name_02,
         to_timestamp, metric_series_datapoints
     );
     total_rows_seeded := total_rows_seeded + seed_metric_series(
-        tenant_schema, metric_id_03, resource_id_03, metric_type_03, metric_name_03,
+        tenant_schema, resource_id_03, metric_type_03, metric_name_03,
         to_timestamp, metric_series_datapoints
     );
     total_rows_seeded := total_rows_seeded + seed_metric_series(
-        tenant_schema, metric_id_04, resource_id_04, metric_type_04, metric_name_04,
+        tenant_schema, resource_id_04, metric_type_04, metric_name_04,
         to_timestamp, metric_series_datapoints
     );
     total_rows_seeded := total_rows_seeded + seed_metric_series(
-        tenant_schema, metric_id_05, resource_id_05, metric_type_05, metric_name_05,
+        tenant_schema, resource_id_05, metric_type_05, metric_name_05,
         to_timestamp, metric_series_datapoints
     );
     total_rows_seeded := total_rows_seeded + seed_metric_series(
-        tenant_schema, metric_id_06, resource_id_06, metric_type_06, metric_name_06,
+        tenant_schema, resource_id_06, metric_type_06, metric_name_06,
         to_timestamp, metric_series_datapoints
     );
     total_rows_seeded := total_rows_seeded + seed_metric_series(
-        tenant_schema, metric_id_07, resource_id_07, metric_type_07, metric_name_07,
+        tenant_schema, resource_id_07, metric_type_07, metric_name_07,
         to_timestamp, metric_series_datapoints
     );
     total_rows_seeded := total_rows_seeded + seed_metric_series(
-        tenant_schema, metric_id_08, resource_id_08, metric_type_08, metric_name_08,
+        tenant_schema, resource_id_08, metric_type_08, metric_name_08,
         to_timestamp, metric_series_datapoints
     );
     total_rows_seeded := total_rows_seeded + seed_metric_series(
-        tenant_schema, metric_id_09, resource_id_09, metric_type_09, metric_name_09,
+        tenant_schema, resource_id_09, metric_type_09, metric_name_09,
         to_timestamp, metric_series_datapoints
     );
     total_rows_seeded := total_rows_seeded + seed_metric_series(
-        tenant_schema, metric_id_10, resource_id_10, metric_type_10, metric_name_10,
+        tenant_schema, resource_id_10, metric_type_10, metric_name_10,
         to_timestamp, metric_series_datapoints
     );
 
