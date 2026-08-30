65b6598f168da4040b75d2888918c4b0ed0d1ad9
g3rard-j <jordaangerard10@gmail.com>
2026-08-30 17:15:42 +0200

    Update historical metric load test to use downsample endpoint

--- nfr-tests/README.md
+++ nfr-tests/README.md
@@ -4,12 +4,13 @@
 docker run --rm \
   --user "$(id -u):$(id -g)" \
   --volume "$(pwd):/apps" \
+  --workdir /apps \
   --interactive \
   --add-host host.docker.internal:host-gateway \
   --env K6_WEB_DASHBOARD=true \
   --env K6_WEB_DASHBOARD_PORT=-1 \
   --env K6_WEB_DASHBOARD_EXPORT=/apps/report.html \
-  grafana/k6 run - < historical-metric-load.js
+  grafana/k6 run /apps/historical-metric-load.js
 ```
 
 ## NFR Test User
--- nfr-tests/historical-metric-load.js
+++ nfr-tests/historical-metric-load.js
@@ -1,6 +1,7 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 import { loginSetup } from './common/utils/login-setup.js';
+import { config } from './common/utils/config.js';
 
 const BASE_URL = __ENV.BASE_URL ?? 'http://host.docker.internal:8083';
 
@@ -12,15 +13,71 @@ export const options = {
   duration: '30s',
 };
 
+
+const queryObjects = [
+  {
+    resourceId: "10000000-0000-0000-0000-000000000001",
+    metricName: "CPUUtilization"
+  },
+  {
+    resourceId: "10000000-0000-0000-0000-000000000002",
+    metricName: "NetworkIn"
+  },
+  {
+    resourceId: "10000000-0000-0000-0000-000000000003",
+    metricName: "NetworkOut"
+  },
+  {
+    resourceId: "10000000-0000-0000-0000-000000000004",
+    metricName: "DiskReadBytes"
+  },
+  {
+    resourceId: "10000000-0000-0000-0000-000000000005",
+    metricName: "DiskWriteBytes"
+  },
+  {
+    resourceId: "10000000-0000-0000-0000-000000000006",
+    metricName: "DatabaseConnections"
+  },
+  {
+    resourceId: "10000000-0000-0000-0000-000000000007",
+    metricName: "ReadLatency"
+  },
+  {
+    resourceId: "10000000-0000-0000-0000-000000000008",
+    metricName: "Invocations"
+  },
+  {
+    resourceId: "10000000-0000-0000-0000-000000000009",
+    metricName: "CPUUtilization"
+  },
+  {
+    resourceId: "10000000-0000-0000-0000-000000000010",
+    metricName: "CPUUtilization"
+  }
+]
+
+const timeWindow = {
+  from: thirtyDaysBack,
+  to: now
+}
+
 export function setup() {
     return loginSetup();
 }
+
 
 export default function (data) { // NOSONAR how k6 expects it
   const jar = http.cookieJar();
   jar.set(`${BASE_URL}`, 'auth_token', data.authToken);
 
-  let res = http.get(`${BASE_URL}/analytics/historical?from=${thirtyDaysBack}&to=${now}&interval=daily`);
-  check(res, { "status is 200": (res) => res.status === 200 });
+  const responses = http.batch(
+    queryObjects.map((object) => ['POST', `${BASE_URL}/analytics/downsampled-historical-series`, JSON.stringify({...object, ...timeWindow}), config.basicJsonHeaderParams])
+  );
+
+  responses.forEach((response) => {
+    check(response, { "status is 200": (response) => response.status === 200 })
+  })
+
   sleep(1);
 }
