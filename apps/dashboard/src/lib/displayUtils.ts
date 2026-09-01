eca04a1e808b0e722ed3c8b7d9935c8cf39b3048
g3rard-j <jordaangerard10@gmail.com>
2026-09-01 09:07:11 +0200

    Add generic display sanitize function and use for usage intelligence linechart

---
+++ apps/dashboard/src/lib/displayUtils.ts
@@ -0,0 +1,32 @@
+import { timeMs } from "./timeUtils";
+
+const GAP_THRESHOLD_MILLISECONDS = timeMs.minuteMs * 5;
+
+export function sanitizeDisplaySeries<T>(
+    dirtyValues: T[],
+    timestampMs: (point: T) => number,
+    buildPadFromCurrentPoint: (samplePoint: T) => T
+): T[] {
+    const sanatizedSeries: T[] = [];
+
+    for (let i = 0; i < dirtyValues.length - 1; i++) {
+        const current = dirtyValues[i];
+        const next = dirtyValues[i + 1];
+
+        sanatizedSeries.push(current);
+
+        if (timestampMs(next) - timestampMs(current) > GAP_THRESHOLD_MILLISECONDS) {
+            sanatizedSeries.push(buildPadFromCurrentPoint(current));
+        }
+    }
+
+    const lastMetric = dirtyValues.at(-1);
+
+    if (lastMetric !== undefined) {
+        sanatizedSeries.push(lastMetric);
+    }
+
+    console.log(sanatizedSeries);
+
+    return sanatizedSeries;
+}
