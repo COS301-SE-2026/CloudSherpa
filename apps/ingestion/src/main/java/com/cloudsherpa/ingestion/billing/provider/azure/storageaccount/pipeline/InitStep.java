1f7cab172465c23439ec5fef12d0a34f2bc8ce9e
g3rard-j <jordaangerard10@gmail.com>
2026-09-08 12:16:31 +0200

    Get credentials & billing export configs for azure billing init step

--- apps/ingestion/src/main/java/com/cloudsherpa/ingestion/billing/provider/azure/storageaccount/pipeline/InitStep.java
+++ apps/ingestion/src/main/java/com/cloudsherpa/ingestion/billing/provider/azure/storageaccount/pipeline/InitStep.java
@@ -1,21 +1,79 @@
 package com.cloudsherpa.ingestion.billing.provider.azure.storageaccount.pipeline;
 
+import com.cloudsherpa.ingestion.billing.BillingExportConfigService;
 import com.cloudsherpa.ingestion.billing.BillingIngestionPipelineStep;
 import com.cloudsherpa.ingestion.billing.provider.azure.storageaccount.AzureBillingContext;
 import com.cloudsherpa.ingestion.connector.CloudCredentials;
-import com.cloudsherpa.lib.entities.AzureBillingExportConfig;
+import com.cloudsherpa.ingestion.scheduler.encryption.CredentialEncryptionService;
+import com.cloudsherpa.lib.entities.BillingExportConfig;
+import com.cloudsherpa.lib.entities.CloudCredential;
+import com.cloudsherpa.lib.repositories.CloudCredentialRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class InitStep implements BillingIngestionPipelineStep<AzureBillingContext> {
+
+  private final CloudCredentialRepository cloudCredentialRepository;
+  private final CredentialEncryptionService encryptionService;
+  private final BillingExportConfigService billingExportConfigService;
+
+  private Logger logger = LoggerFactory.getLogger(InitStep.class);
+
+  public InitStep(
+      CloudCredentialRepository cloudCredentialRepository,
+      CredentialEncryptionService encryptionService,
+      BillingExportConfigService billingExportConfigService) {
+    this.cloudCredentialRepository = cloudCredentialRepository;
+    this.encryptionService = encryptionService;
+    this.billingExportConfigService = billingExportConfigService;
+  }
+
   public void execute(AzureBillingContext context) {
     // Load config + credentials from DB + populate context
-
+    setCredentials(context);
+    setExportConfig(context);
   }
 
-  private CloudCredentials getCredentials() {
-    return null;
+  private void setCredentials(AzureBillingContext context) {
+    try {
+
+      BillingExportConfig billingExportConfig =
+          billingExportConfigService.getBillingExportConfig(context.getConfigId());
+
+      List<CloudCredential> repoCloudCredentials =
+          cloudCredentialRepository.findByAccountIdAndProvider(
+              billingExportConfig.getAccountId(), "AZURE");
+
+      if (repoCloudCredentials.isEmpty()) {
+        throw new IllegalStateException(
+            String.format(
+                "No credentials found for user %s and account %s",
+                context.getUserId(), billingExportConfig.getAccountId()));
+      }
+
+      ObjectMapper objectMapper = new ObjectMapper();
+      CloudCredential credential = repoCloudCredentials.get(0);
+      String decrypted = encryptionService.decrypt(credential.getCredentialValue());
+      context.setCredentials(objectMapper.readValue(decrypted, CloudCredentials.class));
+    } catch (JsonProcessingException e) {
+      throw new IllegalStateException(e);
+    }
   }
 
-  private AzureBillingExportConfig getExportConfig() {
-    return null;
+  private void setExportConfig(AzureBillingContext context) {
+    try {
+      context.setExportConfig(
+          billingExportConfigService.getAzureBillingExportConfig(context.getConfigId()));
+    } catch (NoSuchElementException e) {
+      logger.error(
+          "Azure Billing Export Config with Config ID {} does not exist for user {}",
+          context.getConfigId(),
+          context.getUserId());
+      throw e;
+    }
   }
 }
