35e96c97611f15559240989f006ed3281c4b4a91
Karishma <u23538199@tuks.co.za>
2026-09-08 18:03:15 +0200

    added help button to gcp

--- apps/dashboard/src/features/connectionManager/components/connectionManager/gcp/wizardSetup.tsx
+++ apps/dashboard/src/features/connectionManager/components/connectionManager/gcp/wizardSetup.tsx
@@ -7,6 +7,7 @@ import StepThreeGcp from "./stepThree";
 import type { GcpBillingConfigType } from "./validTypes";
 import { GcpCredentialsDto } from "@/lib/fetch/dto/cloud-credentials";
 import { ResourceDetail } from "@/lib/fetch/dto/cloud-resource";
+import {HelpButton} from "../helpButton";
 
 interface WizardData {
     credentials: GcpCredentialsDto | null;
@@ -98,6 +99,8 @@ export default function WizardSetupGcp() {
                     onBack={handleBack}
                 />
             )}
+
+            <HelpButton provider = "gcp"/>
         </>
     );
 }
