4f4302c9897016b3e171adb1b9939f7e4e385022
Karishma <u23538199@tuks.co.za>
2026-09-09 17:23:14 +0200

    corrected error

--- apps/dashboard/src/features/helpMenu/documents/troubleshooting.tsx
+++ apps/dashboard/src/features/helpMenu/documents/troubleshooting.tsx
@@ -104,6 +104,6 @@ function ContentForTroubleshooting(){
 
 export default function Troubleshooting(){
     return(
-        <Suspense fallback = {<div className = "flex items-center justify-center min-h-screen"> Loading... </div>}> </Suspense>
+        <Suspense fallback = {<div className = "flex items-center justify-center min-h-screen"> Loading... </div>}> <ContentForTroubleshooting/> </Suspense>
     );
 }
