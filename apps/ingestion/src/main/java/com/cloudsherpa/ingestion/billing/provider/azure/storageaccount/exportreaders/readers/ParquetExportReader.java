cd69cfca7cb1ad43a189768aeda1b6d64c84cd8e
g3rard-j <jordaangerard10@gmail.com>
2026-09-14 20:24:26 +0200

    Convert Azure parquet cost export row to intermediate RawBillingRow & batched reads

--- apps/ingestion/src/main/java/com/cloudsherpa/ingestion/billing/provider/azure/storageaccount/exportreaders/readers/ParquetExportReader.java
+++ apps/ingestion/src/main/java/com/cloudsherpa/ingestion/billing/provider/azure/storageaccount/exportreaders/readers/ParquetExportReader.java
@@ -6,10 +6,14 @@ import com.cloudsherpa.ingestion.billing.deserialization.parquet.ParquetReaderSe
 import com.cloudsherpa.ingestion.billing.provider.azure.storageaccount.exportreaders.exeptions.ExportReaderException;
 import com.cloudsherpa.ingestion.billing.provider.azure.storageaccount.model.RawBillingRow;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import org.apache.avro.AvroRuntimeException;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.parquet.hadoop.ParquetReader;
 import org.slf4j.Logger;
@@ -38,7 +42,24 @@ public class ParquetExportReader implements ExportReader<RawBillingRow> {
 
   @Override
   public List<RawBillingRow> readBatch(int maxRows) throws IOException {
-    return List.of();
+
+    List<RawBillingRow> batch = new ArrayList<>();
+
+    while (batch.size() < maxRows) {
+      GenericRecord billingRecord = reader.read();
+
+      if (billingRecord == null) {
+        break;
+      }
+
+      try {
+        batch.add(readRow(billingRecord));
+      } catch (AvroRuntimeException e) {
+        logger.error("Bad row, SKIPPING", e);
+      }
+    }
+
+    return batch;
   }
 
   @Override
@@ -64,5 +85,34 @@ public class ParquetExportReader implements ExportReader<RawBillingRow> {
   private Path parseBlobFilename(String blobName) {
     String[] splitBlobName = blobName.split("/");
     return TEMP_DIRECTORY.resolve(splitBlobName[splitBlobName.length - 1]);
+  }
+
+  private RawBillingRow readRow(GenericRecord billingRecord) {
+    String billingAccountId = billingRecord.get("billingAccountId").toString();
+    LocalDate date =
+        LocalDate.ofEpochDay(
+            (Integer) billingRecord.get("date")); // Assumption, yet to be validated
+    String consumedService = billingRecord.get("consumedService").toString();
+    String meterCategory = billingRecord.get("meterCategory").toString();
+    String meterSubCategory = billingRecord.get("meterSubCategory").toString();
+    String resourceId = billingRecord.get("ResourceId").toString();
+    String chargeType = billingRecord.get("chargeType").toString();
+    String billingCurrency = billingRecord.get("billingCurrency").toString();
+    BigDecimal costInPricingCurrency =
+        new BigDecimal(
+            billingRecord
+                .get("costInPricingCurrency")
+                .toString()); // Assumption, yet to be validated
+
+    return new RawBillingRow(
+        billingAccountId,
+        date,
+        consumedService,
+        meterCategory,
+        meterSubCategory,
+        resourceId,
+        chargeType,
+        billingCurrency,
+        costInPricingCurrency);
   }
 }
