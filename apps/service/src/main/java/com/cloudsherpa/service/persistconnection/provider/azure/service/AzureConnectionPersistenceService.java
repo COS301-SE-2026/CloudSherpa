85633721a766587c6c61d6f1073cd0e6e1f15118
CheriseHeyl <cheriseheyl@gmail.com>
2026-09-14 12:41:38 +0200

    Made default metric history time 14 days

--- apps/service/src/main/java/com/cloudsherpa/service/persistconnection/provider/azure/service/AzureConnectionPersistenceService.java
+++ apps/service/src/main/java/com/cloudsherpa/service/persistconnection/provider/azure/service/AzureConnectionPersistenceService.java
@@ -84,7 +84,7 @@ public class AzureConnectionPersistenceService extends ConnectionPersistenceServ
             .ingestionPeriod(request.ingestionPeriod().toString())
             .createdAt(now)
             .lastBillingIngestion(now)
-            .lastUsageIngestion(now)
+            .lastUsageIngestion(now.minusDays(14))
             .nextUsageIngestion(OffsetDateTime.now(ZoneOffset.UTC).plusMinutes(1))
             .nextBillingIngestion(OffsetDateTime.now(ZoneOffset.UTC).plusMinutes(1))
             .build();
