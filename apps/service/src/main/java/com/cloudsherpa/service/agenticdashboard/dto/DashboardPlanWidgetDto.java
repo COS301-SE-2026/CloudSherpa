db70b594c6216aa794da1fb750f84a28099acf7a
CheriseHeyl <cheriseheyl@gmail.com>
2026-09-20 17:12:36 +0200

    Added dashboard plan dtos

---
+++ apps/service/src/main/java/com/cloudsherpa/service/agenticdashboard/dto/DashboardPlanWidgetDto.java
@@ -0,0 +1,26 @@
+package com.cloudsherpa.service.agenticdashboard.dto;
+
+import com.cloudsherpa.lib.entities.ChartColourEnum;
+import com.cloudsherpa.lib.entities.ChartTypeEnum;
+import com.cloudsherpa.lib.entities.ProviderEnum;
+import com.cloudsherpa.lib.entities.TypeEnum;
+import java.util.UUID;
+
+public record DashboardPlanWidgetDto(
+    UUID widgetId,
+    TypeEnum widgetType,
+    String displayName,
+    Integer startX,
+    Integer startY,
+    Integer width,
+    Integer height,
+    ChartTypeEnum chartType,
+    ChartColourEnum chartColour,
+    ProviderEnum provider,
+    String title,
+    UUID accountId,
+    UUID resourceId,
+    String metricType,
+    String metricName,
+    String[] chargeIds,
+    Integer aggregationWindowDays) {}
