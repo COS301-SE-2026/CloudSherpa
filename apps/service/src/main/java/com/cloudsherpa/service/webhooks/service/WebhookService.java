0a5a7b92261544158e8be3ad1e610baeb88689d2
g3rard-j <jordaangerard10@gmail.com>
2026-09-20 11:19:00 +0200

    Ensure webhook no cloud account means that the webhook is subscribed to events from all cloud accounts

--- apps/service/src/main/java/com/cloudsherpa/service/webhooks/service/WebhookService.java
+++ apps/service/src/main/java/com/cloudsherpa/service/webhooks/service/WebhookService.java
@@ -57,7 +57,7 @@ public class WebhookService {
             request.name(),
             request.endpointUrl(),
             request.eventTypes(),
-            request.cloudAccounts(),
+            normalizeCloudAccounts(request.cloudAccounts()),
             WebhookStatusEnum.ACTIVE,
             webhookKey);
     webhookRepository.save(webhook);
@@ -73,7 +73,7 @@ public class WebhookService {
       webhook.setEndpointUrl(request.endpointUrl());
       webhook.setEventTypes(request.eventTypes());
       webhook.setWebhookStatus(request.status());
-      webhook.setCloudAccounts(request.cloudAccounts());
+      webhook.setCloudAccounts(normalizeCloudAccounts(request.cloudAccounts()));
 
       webhookRepository.save(webhook);
     } catch (NoSuchElementException e) {
@@ -120,5 +120,13 @@ public class WebhookService {
       throw new IllegalStateException(
           "Algorithm for generating webhook HMAC keys does not exist", e);
     }
+  }
+
+  private List<UUID> normalizeCloudAccounts(List<UUID> cloudAccounts) {
+    if (cloudAccounts == null) {
+      return List.of();
+    }
+
+    return cloudAccounts;
   }
 }
