f703bbd9a160d5312a4eb363b892acd159ab04af
g3rard-j <jordaangerard10@gmail.com>
2026-09-22 17:36:50 +0200

    Exclude dev webhook event from registry when dev profile not active

--- apps/service/src/main/java/com/cloudsherpa/service/webhooks/WebhookEventDefinitionRegistry.java
+++ apps/service/src/main/java/com/cloudsherpa/service/webhooks/WebhookEventDefinitionRegistry.java
@@ -7,15 +7,19 @@ import com.cloudsherpa.service.webhooks.events.WebhookEventDefinition;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WebhookEventDefinitionRegistry {
   private final Map<String, WebhookEventDefinition<?>> definitions;
   private Map<String, Map<String, WebhookEventDto<?>>> eventDefinitionPayload;
+  private final Environment environment;
 
-  public WebhookEventDefinitionRegistry(Map<String, WebhookEventDefinition<?>> definitions) {
+  public WebhookEventDefinitionRegistry(
+      Map<String, WebhookEventDefinition<?>> definitions, Environment environment) {
     this.definitions = definitions;
+    this.environment = environment;
     eventDefinitionPayload = new HashMap<>();
     constructDefinitionPayload();
   }
@@ -26,6 +30,12 @@ public class WebhookEventDefinitionRegistry {
 
   private void constructDefinitionPayload() {
     for (WebhookEventDefinition<?> definition : definitions.values()) {
+
+      // Exclude dev event from registered definitions when dev profile not active
+      if (!environment.matchesProfiles("dev") && definition.type().equals("dev.event")) {
+        continue;
+      }
+
       eventDefinitionPayload
           .computeIfAbsent(definition.category(), k -> new HashMap<>())
           .put(
